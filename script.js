fedc1e29bcbc54c73af8d8c61a1a71e9a57b627f
makinaro <112000847+makinaro@users.noreply.github.com>
2023-11-08 03:26:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var addQuestionButton = document.getElementById("add-question-button");
+    var questionList = document.getElementById("question-list");
+    var warning = document.getElementById("warning");
+
+    addQuestionButton.addEventListener("click", function () {
+        var question = document.getElementById("question").value;
+        var answer = document.getElementById("answer").value;
+
+        if (question === "" || answer === "") {
+            alert("Please fill out both fields.");
+            warning.textContent = "Please fill out both fields.";
+            document.getElementById("question").value = "";
+            document.getElementById("answer").value = "";
+            return;
+        }
+
+        // Create a new row
+        var newRow = questionList.insertRow();
+
+        // Create cells for question and answer
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+
+        // Set the cell contents
+        cell1.textContent = question;
+        cell2.textContent = answer;
+        cell3.innerHTML = "<button class='delete-button'>Delete</button>";
+
+        // Clear the input fields after adding the row
+        document.getElementById("question").value = "";
+        document.getElementById("answer").value = "";
+        warning.textContent = "";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var questionTable = document.getElementById("question-list");
+
+    // Event delegation for handling "Delete" button clicks
+    questionTable.addEventListener("click", function (event) {
+        if (event.target.classList.contains("delete-button")) {
+            var row = event.target.closest("tr"); // Get the closest table row
+            row.remove(); // Remove the row
+        }
+    });
+});
+
